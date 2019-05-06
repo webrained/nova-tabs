44cebbea9fd1042be1c00ed4c29bf9f8c4fb9c12
Arman <arrman4@gmail.com>
2019-05-06 15:22:13 +0400

    relationship tab fix

--- resources/js/components/DetailTabs.vue
+++ resources/js/components/DetailTabs.vue
@@ -4,36 +4,37 @@
       <!--<h4 class="text-90 font-normal text-2xl mb-3">{{ panel.name }}</h4>-->
     </slot>
     <div class="relationship-tabs-panel card">
-      <div class="tabs-wrap border-b-2 border-40 w-full">
-        <div class="tabs flex flex-row overflow-x-auto">
-          <button
-            class="py-5 px-8 border-b-2 focus:outline-none tab"
-            :class="[activeTab == tab.name ? 'text-grey-black font-bold border-primary': 'text-grey font-semibold border-40']"
-            v-for="(tab, key) in tabs"
-            :key="key"
-            @click="handleTabClick(tab, $event)"
-          >{{ tab.name }}</button>
-        </div>
+      <div
+              class="tabs flex flex-row overflow-x-auto flex-wrap flex-wrap"
+              :class="{'has-search-bar': activeTabHasSearch}"
+      >
+        <button
+                class="py-5 px-8 border-b-2 focus:outline-none tab"
+                :class="[activeTab == tab.name ? 'text-grey-black font-bold border-primary': 'text-grey font-semibold border-40']"
+                v-for="(tab, key) in tabs"
+                :key="key"
+                @click="handleTabClick(tab, $event)"
+        >{{ tab.name }}</button>
+        <div class="flex-1 border-b-2 border-40"></div>
       </div>
       <div
-        :class="[(panel && panel.defaultSearch) ? 'default-search': 'tab-content', slugify(tab.name)]"
-        :ref="slugify(tab.name)"
-        v-for="(tab, index) in tabs"
-        v-show="tab.name == activeTab"
-        :label="tab.name"
-        :key="'related-tabs-fields' + index"
+              :class="[(panel && panel.defaultSearch) ? 'default-search': 'tab-content']"
+              v-for="(tab, index) in tabs"
+              v-show="tab.name == activeTab"
+              :label="tab.name"
+              :key="'related-tabs-fields' + index"
       >
         <div :class="{'px-6 py-3':!tab.listable}" v-if="tab.init">
           <component
-            v-for="(field, index) in tab.fields"
-            :class="{'remove-bottom-border': index == tab.fields.length - 1}"
-            :key="'tab-' + index"
-            :is="componentName(field)"
-            :resource-name="resourceName"
-            :resource-id="resourceId"
-            :resource="resource"
-            :field="field"
-            @actionExecuted="actionExecuted"
+                  v-for="(field, index) in tab.fields"
+                  :class="{'remove-bottom-border': index == tab.fields.length - 1}"
+                  :key="'tab-' + index"
+                  :is="componentName(field)"
+                  :resource-name="resourceName"
+                  :resource-id="resourceId"
+                  :resource="resource"
+                  :field="field"
+                  @actionExecuted="actionExecuted"
           />
         </div>
       </div>
@@ -42,163 +43,145 @@
 </template>
 
 <script>
-import BehavesAsPanel from "laravel-nova/src/mixins/BehavesAsPanel";
-export default {
-  mixins: [BehavesAsPanel],
-  data() {
-    return {
-      tabs: null,
-      activeTab: ""
-    };
-  },
-  computed: {
-    activeTabSearchWidth() {
-      if (this.activeTabHasSearch) {
-        let element = this.$el.querySelector(
-          "." + this.slugify(this.activeTab) + " .w-search"
-        );
+  import BehavesAsPanel from "laravel-nova/src/mixins/BehavesAsPanel";
+  export default {
+    mixins: [BehavesAsPanel],
+    data() {
+      return {
+        tabs: null,
+        activeTab: ""
+      };
+    },
+    computed: {
+      activeTabHasSearch() {
+        let tab = _.find(this.tabs, { name: this.activeTab });
+        let hasTab = false;
+
+        if (!tab || this.panel.defaultSearch) {
+          return false;
+        }
+
+        _.forEach(tab.fields, function(field) {
+          if (field.resourceName == "action-events") {
+            return;
+          }
+          if (
+                  field.component == "has-many-field" ||
+                  field.component == "belongs-to-many-field" ||
+                  field.component == "morph-many-field" ||
+                  field.component == "morph-to-field" ||
+                  field.component == "morph-to-many-field"
+          ) {
+            hasTab = true;
+          }
+
+        });
+
+        return hasTab;
       }
     },
-    activeTabHasSearch() {
-      let tab = _.find(this.tabs, { name: this.activeTab });
-      let hasSearch = false;
-
-      if (!tab || this.panel.defaultSearch) {
-        return false;
-      }
-
-      _.forEach(tab.fields, function(field) {
-        if (field.resourceName == "action-events") {
-          return;
+    mounted() {
+      let tabs = {};
+      this.panel.fields.forEach(field => {
+        if (!tabs.hasOwnProperty(field.tab)) {
+          tabs[field.tab] = {
+            name: field.tab,
+            init: false,
+            listable: field.listableTab,
+            fields: []
+          };
         }
-        if (
-          field.component == "has-many-field" ||
-          field.component == "belongs-to-many-field" ||
-          field.component == "morph-many-field" ||
-          field.component == "morph-to-many-field"
-        ) {
-          hasSearch = true;
-        }
+        tabs[field.tab].fields.push(field);
       });
-
-      return hasSearch;
-    }
-  },
-  mounted() {
-    let tabs = {};
-    this.panel.fields.forEach(field => {
-      if (!tabs.hasOwnProperty(field.tab)) {
-        tabs[field.tab] = {
-          name: field.tab,
-          init: false,
-          listable: field.listableTab,
-          fields: []
-        };
+      this.tabs = tabs;
+      this.handleTabClick(tabs[Object.keys(tabs)[0]]);
+    },
+    methods: {
+      /**
+       * Handle the actionExecuted event and pass it up the chain.
+       */
+      actionExecuted() {
+        this.$emit("actionExecuted");
+      },
+      handleTabClick(tab, event) {
+        tab.init = true;
+        this.activeTab = tab.name;
+      },
+      componentName(field) {
+        return field.prefixComponent
+                ? "detail-" + field.component
+                : field.component;
       }
-      tabs[field.tab].fields.push(field);
-    });
-    this.tabs = tabs;
-    if(!_.isUndefined(this.$route.query.tab)) {
-        if(_.isUndefined(tabs[this.$route.query.tab])) {
-            this.handleTabClick(tabs[Object.keys(tabs)[0]]);
-        } else {
-            this.activeTab = this.$route.query.tab;
-            this.handleTabClick(tabs[this.$route.query.tab]);
-        }
-    } else {
-        this.handleTabClick(tabs[Object.keys(tabs)[0]]);
     }
-  },
-  methods: {
-    /**
-     * Handle the actionExecuted event and pass it up the chain.
-     */
-    actionExecuted() {
-      this.$emit("actionExecuted");
-    },
-    handleTabClick(tab, event) {
-      tab.init = true;
-      this.activeTab = tab.name;
-    },
-    /**
-     * Slugify
-     * From: https://gist.github.com/mathewbyrne/1280286
-     */
-    slugify(text) {
-      return text
-        .toString()
-        .toLowerCase()
-        .replace(/\s+/g, "-") // Replace spaces with -
-        .replace(/[^\w\-]+/g, "") // Remove all non-word chars
-        .replace(/\-\-+/g, "-") // Replace multiple - with single -
-        .replace(/^-+/, "") // Trim - from start of text
-        .replace(/-+$/, ""); // Trim - from end of text
-    },
-    componentName(field) {
-      return field.prefixComponent
-        ? "detail-" + field.component
-        : field.component;
-    }
-  }
-};
+  };
 </script>
 
 
 <style lang="scss">
-.relationship-tabs-panel {
-  .has-search-bar {
-  }
-  .tabs::-webkit-scrollbar {
-    height: 8px;
-    border-radius: 4px;
-  }
-  .tabs::-webkit-scrollbar-thumb {
-    background: #cacaca;
-  }
-  .tabs {
-    white-space: nowrap;
-    margin-bottom: -2px;
-  }
-  .card {
-    box-shadow: none;
-  }
-  h1 {
-    display: none;
-  }
-  .tab {
-    padding-top: 1.25rem;
-    padding-bottom: 1.25rem;
-  }
-  .default-search > div > .relative > .flex {
-    justify-content: flex-end;
-    padding-left: 1.25rem;
-    padding-right: 1.25rem;
-    margin-top: 1.25rem;
-    margin-bottom: 0;
-    > .mb-6 {
-      margin-bottom: 0;
-    }
-  }
-  .tab-content > div > .relative > .flex {
-    justify-content: flex-end;
-    padding-left: 0.75rem;
-    padding-right: 0.75rem;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-107px);
-    align-items: center;
-    height: 62px;
-    z-index: 1;
+  .relationship-tabs-panel {
+    .has-search-bar {
+      margin-bottom: 86px;
 
-    > .mb-6 {
-      margin-bottom: 0;
+    }
+    .tabs::-webkit-scrollbar {
+      width: 5px;
+      height: 5px;
+    }
+    .tabs::-webkit-scrollbar-thumb {
+      background: #eef1f4;
+    }
+    .tabs {
+      white-space: nowrap;
+      margin-bottom: -2px;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      flex-grow: 1;
+    }
+    .card {
+      box-shadow: none;
     }
 
-    > .w-full {
-      width: auto;
-      margin-left: 1.5rem;
+    h1 {
+      display: none;
+    }
+
+    .tab {
+      padding-top: 1.25rem;
+      padding-bottom: 1.25rem;
+    }
+
+    .default-search > div > .relative > .flex {
+      justify-content: flex-end;
+      padding-left: 1.25rem;
+      padding-right: 1.25rem;
+      margin-top: 1.25rem;
+      margin-bottom: 0;
+      > .mb-6 {
+        margin-bottom: 0;
+      }
+    }
+
+    .tab-content > div > .relative > .flex {
+      justify-content: flex-end;
+      padding-left: 0.75rem;
+      padding-right: 0.75rem;
+
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translateY(-100%);
+
+      align-items: center;
+      height: 62px;
+
+      > .mb-6 {
+        margin-bottom: 0;
+      }
+
+      > .w-full {
+        width: auto;
+        margin-left: 1.5rem;
+      }
     }
   }
-}
 </style>
